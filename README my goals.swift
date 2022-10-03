7553c4561c9f3269540e72a03f3aa0f1d835a73b
roco <roco10000hours@gmail.com>
2022-10-03 21:54:23 +0900

    click

--- README my goals.swift
+++ README my goals.swift
@@ -1,10 +1,10 @@
-let mygoals: String = """
+let myGoals: String = """
 I'WILL not immersed in technology, but will value the people themselves.
 I'm aiming to be a developer. 
 First of all, basic IT knowledge and specialized technologies
-It's an essential element
+It's an essential element to Me.
 For the time being, I'm going to focus on building knowledge.
 After that, I'M going to go out to the field and fill it in as he gains experience
 """
 
-print(mygoals)
+print(myGoals)
