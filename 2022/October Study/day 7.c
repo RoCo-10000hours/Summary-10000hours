0ac87ae477247c8f9936c2cfcf534b4918f76346
roco <roco10000hours@gmail.com>
2022-10-13 09:03:50 +0900

    click

--- 2022/October Study/day 7.c
+++ 2022/October Study/day 7.c
@@ -42,4 +42,72 @@ myInstance.myMethod(name: "park", age: 13)
 [myInstance myMethodWithName:@"park", age: 13]
 
                                                 my ppt ~167page
-//-------------------------------------------------------------
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+<< 메서드 정의 (2) >>
+
+- 1) 반환유형 : '' return_type ''은 함수가 반환하는 값의 데이터 유형,
+                일부메서드등은 값을 반환하지않고도 작업을 수행하는데 이럴땐
+                return_type의 키워드는 '' void ''
+- 2) 메서드이름 
+- 3) 인수(Argument) : 함수가 호출되면 인수에 값을 전달함. 매개변수라고도함
+- 4) 결합인수(joining argument) : 호출하는 동안 읽기 쉽고 명확하게하기 위함
+- 5) 메서드본문 : 메서드가 수행하는 작업을 정의하는 명령문의 모음이 포함됨
+
+-(int) max:(int) num1 secondNumber:(int) num2 {
+    int result;
+    if (num1 > num2) {
+        result = num1;
+    } else {
+        reusult = num2;
+    
+    }
+}
+    return result;
+//위의 코드는 2개의 매개변수 num1,2를 사용하고 두 매개변수 사이의 최대값을 반환
+
+//Obective-C와 Swift의 비교 @@@@
+- (void)myMethodWithMynaem: (NSString *)name myAge: (int)age {
+                //name활용
+                ///age활용
+}
+[myInstance myMethodWithMynaem:@"park", myAge:20]
+//ㅡㅡㅡㅡㅡㅡㅡㅡ
+func myMethod(myName name: String, myAGE age: Int) {
+    print("\(nmae) \(age)")
+}  
+myInstance.myMethod(myName: "park",myAGE: 20)
+  
+  
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+<< 메서드선언method declarations >>
+
+- 컴파일러에게 함수 이름꽈 메서드를 호출하는 방법을 알려줌
+// 실제 본문은 별도로 정의할 수 있다
+-(return_type)function_name:(argumentType1)argumentName1
+joiningArgument2:(argumentType2)argumentName2 ...
+joiningArgumentn:(argumentTypen)argumentNamen;
+
+// 한 소스파일에서 메서드를 정의하고 다르파일에서 해당 메서드 호추시 선언이 필요
+    // (이 경우엔 함수를 호출하는 파일의 맨 위에 함수를 선언해야한다)
+-(int) max:(int)num1 andNum2:(int)num2;
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+<< 메서드호출 calling a method >>
+
+- 옵젝c메서드를 생성하는 동안 함수가 수행해야 하는 작업에 대한 정의를 제공함
+- 메서드를 사용하려면 정의된 작업을 수행하기위해 해당함수를 호출필요
+- (프로그램이 함수 호출 ---> (프로그램제어)---> 메서드로 전달(메서드호출)
+          --->정의된 작업수행 --> 반환문 실행 or 함수종료중괄호도달시 
+         ---> (프로그램제어) ---> 주프로그램반환 )
+- 매서드를 호출하려면 매서드이름과함께 필요한 매개변수를 전달하기만하며되고
+  메서드가 값을 반환하면 반환된 값을 저장가능.  
+  
+                                                            my ppt~ 171page
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+<< 메서드호출 calling a method(2) >>
